f6981168fb2eed6834aa6f4a36453cf1fa6a5c8a
jemzir <nghiemj23@gmail.com>
2022-11-15 14:11:18 -0800

    set up for webpack and configuration of it

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './main/client/index.js'),
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /.css$/i,
+        use: ['style-loader','css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './main/client/index.html'
+    })
+  ],
+  devServer: {
+    statis: {
+      directory: path.resolve(__dirname, './build')
+    }
+  }
+}
