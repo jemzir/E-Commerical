758467b6525e4526564372edb516ba2a3fb2dc37
jemzir <nghiemj23@gmail.com>
2022-11-15 15:28:50 -0800

    began boilerplate set up for serverside

--- main/server/routes/api.js
+++ main/server/routes/api.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const commerceController = require('../controllers/commerce-controllers');
+const router = express.Router();
+
+/**
+ * have a router each for CRUD functionality...
+ * 
+ */
+
+// functionality needs to add product data...
+
+// functionality needs to get product data...
+router.get('/', commerceController); 
+
+// functionality needs to update product data...
+
+// functionality needs to delete product data...
+
+
+
+
