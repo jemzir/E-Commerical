c1fcb543a7bc63495366d739a026b1416a45e51d
jemzir <nghiemj23@gmail.com>
2022-11-16 22:25:24 -0800

    creation and finish of CRUD front to back

---
+++ main/client/components/updateProduct.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function UpdateProduct() {
+  const clickHandler = async () => {
+    console.log('update btn clicked');
+    const inputTagArr = document.querySelectorAll('.upd');
+    const reqBod = {
+      "product_id": +inputTagArr[0].value,
+      "product_name": inputTagArr[1].value,
+      "product_price": +inputTagArr[2].value,
+      "product_details": inputTagArr[3].value
+    };
+    console.log('upd reqBod:', reqBod);
+
+    const result = await fetch('/api', {
+      method:'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(reqBod)
+    });
+    console.log('result of fetchUpdate: ', result);
+
+    inputTagArr.forEach((tag) => {
+      tag.value = ''
+    });
+  }
+
+  return (
+    <div>
+      <input id="prod_id" className="upd" placeholder="ID number to update"></input>
+      <input id="name" className="upd" placeholder="name"></input>
+      <input id="price" className="upd" placeholder="price"></input>
+      <input id="details" className="upd" placeholder="details -- limited 255 characters"></input>
+      <button onClick={clickHandler}>Update Product Info</button>
+      <br></br>
+    </div>
+  )
+}
+
+export default UpdateProduct;
